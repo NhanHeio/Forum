7752a4ec19d6ea5465ba3b850e1ae8b5c6fd1bd5
Ho Trung Nhan <hotrungnhan2000@gmail.com>
2021-10-31 12:37:00 +0700

    reconstruct

---
+++ bottom.php
@@ -0,0 +1,35 @@
+<div id="footer">
+            <div class="row">
+                <div class="col-4 footer-authors">
+                    <h3 class="footer-heading">Author</h3>
+                    <ul style="list-style-type: none;padding-left:0;">
+                        <li class="footer-author">Hồ Trung Nhân</li>
+                        <li class="footer-author">Võ Văn Khánh</li>
+                        <li class="footer-author">Đinh Hiếu Nghĩa</li>
+                    </ul>
+                </div>
+                <div class="col-4 footer-links">
+                    <h3 class="footer-heading">Contact</h3>
+                    <ul style="list-style-type: none;padding-left:0;">
+                        <li><a class="footer-link" href="https://www.facebook.com/nhantrung.ho/">
+                            <i class="fab fa-facebook"></i>
+                            Facebook
+                        </a></li>
+                        <li><a class="footer-link" href="https://www.instagram.com/nhantrung_v.i.p/">
+                            <i class="fab fa-instagram"></i>
+                            Instagram
+                        </a></li>
+                        <li><a class="footer-link" href="#">
+                            <i class="fal fa-envelope"></i>
+                            Gmail
+                        </a></li>
+                    </ul>
+                </div>
+                <div class="col-4 footer-privacy">
+                    <a href="#" class="footer-link">Privacy Policy</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    
